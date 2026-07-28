e62039785422e34ebb9d1a69a48c285118e6bf4f
andriux26 <andriux26@gmail.com>
2026-07-28 17:55:10 +0300

    Add Arduino GFX color compatibility header

---
+++ RX_FSK/color_compat.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#ifndef BLACK
+#define BLACK 0x0000
+#endif
+#ifndef WHITE
+#define WHITE 0xFFFF
+#endif
+#ifndef RED
+#define RED 0xF800
+#endif
+#ifndef GREEN
+#define GREEN 0x07E0
+#endif
+#ifndef BLUE
+#define BLUE 0x001F
+#endif
+#ifndef CYAN
+#define CYAN 0x07FF
+#endif
+#ifndef MAGENTA
+#define MAGENTA 0xF81F
+#endif
+#ifndef YELLOW
+#define YELLOW 0xFFE0
+#endif
