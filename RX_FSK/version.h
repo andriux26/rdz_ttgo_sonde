e3d356868c089ba32e7837e371ceffe388758be4
andriux26 <andriux26@gmail.com>
2026-07-30 21:05:17 +0300

    Update version.h

--- RX_FSK/version.h
+++ RX_FSK/version.h
@@ -1,4 +1,4 @@
-const char *version_name = "rdzTTGOsonde";
-const char *version_id = "dev20250455";
+const char *version_name = "sonde";
+const char *version_id = "dev2026";
 const int FS_MAJOR=3;
 const int FS_MINOR=3;
